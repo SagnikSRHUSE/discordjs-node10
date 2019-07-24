b73e35e33f21244d96ce4dc463a9fbac178e845a
Sagnik Sasmal <16162562+SagnikSRHUSE@users.noreply.github.com>
2019-07-25 04:43:34 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,7 @@
 sleep 2
 
 cd /home/container
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 
 # Run the Server
 npm install && npm audit fix
-${MODIFIED_STARTUP}
+${STARTUP}

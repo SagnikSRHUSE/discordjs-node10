ad9520c63cc5779171f1b3dea7c209cc5d880847
Sagnik Sasmal <16162562+SagnikSRHUSE@users.noreply.github.com>
2019-07-25 06:22:55 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ cd /home/container
 
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo ":/home/container$ ${STARTUP}"
+echo "${MODIFIED_STARTUP}"
 
 # Run the Server
 npm install --quiet --no-progress --loglevel=error && npm audit fix --quiet --no-progress --loglevel=error
